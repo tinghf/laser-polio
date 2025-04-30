8ca3dec229e5744313e809537afcb9909dc6b283
Steve Kroiss <55462700+skroiss-idm@users.noreply.github.com>
2025-04-29 17:39:04 -0700

    setting default max_migr_frac to 1

--- src/laser_polio/pars.py
+++ src/laser_polio/pars.py
@@ -41,7 +41,7 @@ default_pars = PropertySet(
         "gravity_a": 1,  # Origin population exponent
         "gravity_b": 1,  # Destination population exponent
         "gravity_c": 2.0,  # Distance exponent
-        "max_migr_frac": 0.3,  # Max fraction of population that migrates
+        "max_migr_frac": 1.0,  # Max fraction of population that migrates
         "node_lookup": None,  # Node info (node_id are keys, dict contains dot_name, lat, lon)
         # Interventions
         "vx_prob_ri": None,  # Should include coverage and efficacy from expected number of RI doses

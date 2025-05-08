abd53f4f13f34748adb730df42015fd38b8a5ec3
skroiss-idm <skroiss-idm@users.noreply.github.com>
2025-05-08 18:45:28 +0000

    Bump version: 0.1.9 → 0.1.10

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ project = "LASER Polio (PHASER)"
 year = "2025"
 author = "Institute for Disease Modeling"
 copyright = f"{year}, Gates Foundation"
-version = release = "0.1.9"
+version = release = "0.1.10"
 
 pygments_style = "trac"
 templates_path = ["."]
--- src/laser_polio/__init__.py
+++ src/laser_polio/__init__.py
@@ -7,6 +7,6 @@ from pathlib import Path
 # from .seir_mpm import *
 from .utils import *  # noqa F403
 
-__version__ = "0.1.9"
+__version__ = "0.1.10"
 
 root = Path(__file__).resolve().parents[2]

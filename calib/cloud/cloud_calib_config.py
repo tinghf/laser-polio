44e067cf9d9f1fe24e599272e0096471bf75155e
Steve Kroiss <55462700+skroiss-idm@users.noreply.github.com>
2025-05-22 16:05:58 -0700

    new calib

--- calib/cloud/cloud_calib_config.py
+++ calib/cloud/cloud_calib_config.py
@@ -5,20 +5,23 @@ import yaml
 
 # ------------------- USER CONFIGS -------------------
 
-study_name = "calib_nigeria_3y_underwt_gravity_2018_20250523"
-model_config = "config_nigeria_3y_underwt_gravity_2018.yaml"
+job_name = "laser-polio-worker-sk2"
+study_name = "calib_nigeria_3y_pim_gravity_2018_20250523"
+model_config = "config_nigeria_3y_pim_gravity_2018.yaml"
+# job_name = "laser-polio-worker-sk"
+# study_name = "calib_nigeria_3y_underwt_gravity_2018_20250523"
+# model_config = "config_nigeria_3y_underwt_gravity_2018.yaml"
 calib_config = "r0_k_ssn_gravity.yaml"
 fit_function = "log_likelihood"
 num_trials = 1  # Number of trials to run per pod
 n_replicates = 1  # Number of replicates to run for each trial
-parallelism = 50  # The number of pods (i.e., jobs) to run in parallel
+parallelism = 25  # The number of pods (i.e., jobs) to run in parallel
 completions = 1000  # The total number of pods (i.e., jobs) that need to successfully complete before the job is considered "done"
 
 # ---------------------------------------------------
 
 # Default settings
 namespace = "default"
-job_name = "laser-polio-worker-sk"
 image = "idm-docker-staging.packages.idmod.org/laser/laser-polio:latest"
 
 # Define the path to the YAML file with the storage URL from the docs

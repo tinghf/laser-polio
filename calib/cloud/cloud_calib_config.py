9955f5b656c7e3c53a6adb30d8311dd22af88ed5
Steve Kroiss <55462700+skroiss-idm@users.noreply.github.com>
2025-05-22 20:22:40 -0700

    new calibs

--- calib/cloud/cloud_calib_config.py
+++ calib/cloud/cloud_calib_config.py
@@ -6,14 +6,20 @@ import yaml
 # ------------------- USER CONFIGS -------------------
 
 job_name = "laser-polio-worker-sk"
-study_name = "calib_nigeria_6y_pim_gravity_zinb_20250523"
+study_name = "calib_nigeria_6y_pim_gravity_zinb_initimmunscalar_20250523"
 model_config = "config_nigeria_6y_pim_gravity_zinb.yaml"
-calib_config = "r0_k_ssn_gravity_zinb.yaml"
+calib_config = "r0_k_ssn_gravity_zinb_initimmunscalar.yaml"
+
+# job_name = "laser-polio-worker-sk2"
+# study_name = "calib_nigeria_6y_pim_gravity_zinb_hetero_20250523"
+# model_config = "config_nigeria_6y_pim_gravity_zinb.yaml"
+# calib_config = "r0_k_ssn_gravity_zinb_hetero.yaml"
+
 fit_function = "log_likelihood"
 num_trials = 1  # Number of trials to run per pod
 n_replicates = 1  # Number of replicates to run for each trial
-parallelism = 50  # The number of pods (i.e., jobs) to run in parallel
-completions = 1000  # The total number of pods (i.e., jobs) that need to successfully complete before the job is considered "done"
+parallelism = 25  # The number of pods (i.e., jobs) to run in parallel
+completions = 10000  # The total number of pods (i.e., jobs) that need to successfully complete before the job is considered "done"
 
 # ---------------------------------------------------
 

b6c9fe57f44af9bccacd2d4ff83ba53961ef28f7
Steve Kroiss <55462700+skroiss-idm@users.noreply.github.com>
2025-05-28 10:52:10 -0700

    new calib

--- calib/cloud/cloud_calib_config.py
+++ calib/cloud/cloud_calib_config.py
@@ -23,7 +23,7 @@ calib_config = "r0_k_ssn_gravity_zinb.yaml"
 fit_function = "log_likelihood"
 num_trials = 1  # Number of trials to run per pod
 n_replicates = 1  # Number of replicates to run for each trial
-parallelism = 25  # The number of pods (i.e., jobs) to run in parallel
+parallelism = 50  # The number of pods (i.e., jobs) to run in parallel
 completions = 10000  # The total number of pods (i.e., jobs) that need to successfully complete before the job is considered "done"
 
 # ---------------------------------------------------

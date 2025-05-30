e666f7caaac76c25bbf4d7d4be631ce9d59a921f
Steve Kroiss <55462700+skroiss-idm@users.noreply.github.com>
2025-05-30 11:26:59 -0700

    cleanup

--- calib/cloud/run_report_aks.py
+++ calib/cloud/run_report_aks.py
@@ -38,19 +38,6 @@ def main():
         print("📈 Plotting results...")
         plot_stuff(cfg.study_name, study.storage_url, output_dir=results_path)
 
-        # TODO: plot a timeseries with map below it
-        # # Try loading shapefile for mapping
-        # shp = None
-        # try:
-        #     # Geography
-        #     regions = study.user_attrs["model_config"]["regions"]
-        #     dot_names = lp.find_matching_dot_names(regions, lp.root / "data/compiled_cbr_pop_ri_sia_underwt_africa.csv")
-        #     shp = gpd.read_file(filename="data/shp_africa_low_res.gpkg", layer="adm2")
-        #     shp = shp[shp["dot_name"].isin(dot_names)]
-        #     shp.set_index("dot_name", inplace=True)  # Sort the GeoDataFrame by the order of dot_names
-        #     shp = shp.loc[dot_names].reset_index()
-        # except Exception as e:
-        #     print(f"⚠️ Warning: Could not load shapefile for mapping: {e}")
         print("📊 Plotting target comparisons...")
         plot_targets(study, output_dir=results_path)
 

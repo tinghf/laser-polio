fb00994ba9c789983d7f910f31ec063b1b160c73
Steve Kroiss <55462700+skroiss-idm@users.noreply.github.com>
2025-06-16 13:26:36 -0700

    new calibs

--- calib/cloud/cloud_calib_config.py
+++ calib/cloud/cloud_calib_config.py
@@ -25,20 +25,20 @@ import yaml
 # model_config = "config_nigeria_4y_2020_underwt_gravity_zinb_ipv.yaml"
 # calib_config = "r0_k_ssn_gravitykabc_zinb_r0wtscalars.yaml"
 
-job_name = "laser-polio-worker-sk5"
-study_name = "calib_nigeria_6y_2018_underwt_gravity_zinb_ipv_20250613"
-model_config = "config_nigeria_6y_2018_underwt_gravity_zinb_ipv.yaml"
-calib_config = "r0_k_ssn_gravitykabc_zinb_r0wtscalars.yaml"
-
-# job_name = "laser-polio-worker-sk6"
-# study_name = "calib_nigeria_7y_2017_underwt_gravity_zinb_ipv_20250613"
-# model_config = "config_nigeria_7y_2017_underwt_gravity_zinb_ipv.yaml"
+# job_name = "laser-polio-worker-sk5"
+# study_name = "calib_nigeria_6y_2018_underwt_gravity_zinb_ipv_20250613"
+# model_config = "config_nigeria_6y_2018_underwt_gravity_zinb_ipv.yaml"
 # calib_config = "r0_k_ssn_gravitykabc_zinb_r0wtscalars.yaml"
+
+job_name = "laser-polio-worker-sk6"
+study_name = "calib_nigeria_7y_2017_underwt_gravity_zinb_ipv_20250613"
+model_config = "config_nigeria_7y_2017_underwt_gravity_zinb_ipv.yaml"
+calib_config = "r0_k_ssn_gravitykabc_zinb_r0wtscalars.yaml"
 
 fit_function = "log_likelihood"
 n_trials = 1  # Number of trials to run per pod
 n_replicates = 1  # Number of replicates to run for each trial
-parallelism = 25  # The number of pods (i.e., jobs) to run in parallel
+parallelism = 50  # The number of pods (i.e., jobs) to run in parallel
 completions = 20000  # The total number of pods (i.e., jobs) that need to successfully complete before the job is considered "done"
 
 # ---------------------------------------------------

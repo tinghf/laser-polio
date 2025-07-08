2c8a020288e37190d8f941a645a234d90996960b
skroiss-idm <skroiss-idm@users.noreply.github.com>
2025-07-08 16:58:52 +0000

    Bump version: 0.2.10 → 0.2.11

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ project = "LASER Polio (PHASER)"
 year = "2025"
 author = "Institute for Disease Modeling"
 copyright = f"{year}, Gates Foundation"
-version = release = "0.2.10"
+version = release = "0.2.11"
 
 pygments_style = "trac"
 templates_path = ["."]

b4e0a328d9da680cead7913ef4b0d97d6e6dcf4d
skroiss-idm <steve.kroiss@gatesfoundation.org>
2025-07-16 14:45:19 -0700

    new config

--- calib/cloud/cloud_calib_config.py
+++ calib/cloud/cloud_calib_config.py
@@ -5,59 +5,14 @@ import yaml
 
 # ------------------- USER CONFIGS -------------------
 
-# job_name = "laser-polio-sk1"
-# study_name = "calib_nigeria_6y_2018_underwt_grav_ipv_dirichlet_20250618"
-# model_config = "config_nigeria_6y_2018_underwt_gravity_zinb_ipv.yaml"
-# calib_config = "r0_ssn_gravkabc_zinb_r0sclrs_siasclrs_initimmunsclrs_dirichlet.yaml"
-
-# job_name = "laser-polio-sk2"
-# study_name = "calib_nigeria_6y_2018_underwt_grav_ipv_kanoboost_dirichlet_20250618"
-# model_config = "config_nigeria_6y_2018_underwt_gravity_zinb_ipv_kanoboost.yaml"
-# calib_config = "r0_ssn_gravkabc_zinb_r0sclrs_siasclrs_initimmunsclrs_dirichlet.yaml"
-
-# job_name = "laser-polio-sk3"
-# study_name = "calib_nigeria_6y_2018_pim_grav_ipv_dirichlet_20250618"
-# model_config = "config_nigeria_6y_2018_pim_gravity_zinb_ipv.yaml"
-# calib_config = "r0_ssn_gravkabc_zinb_r0sclrs_siasclrs_initimmunsclrs_dirichlet.yaml"
-
-# job_name = "laser-polio-sk4"
-# study_name = "calib_nigeria_6y_2018_pim_grav_ipv_dirichlet_3periods_earlierandmoreseeds_20250619"
-# model_config = "config_nigeria_6y_2018_underwt_gravity_zinb_ipv_moreseeds.yaml"
-# calib_config = "r0_ssn_gravkabc_zinb_r0sclrs_siasclrs_initimmunsclrs_dirichlet.yaml"
-
-# job_name = "laser-polio-sk5"
-# study_name = "calib_nigeria_6y_2018_pim_grav_ipv_dirichlet_3periods_earlierandmoreseeds_immunadj_20250619"
-# model_config = "config_nigeria_6y_2018_underwt_gravity_zinb_ipv_moreseeds_immunadj.yaml"
-# calib_config = "r0_ssn_gravkabc_zinb_r0sclrs_siasclrs_initimmunsclrs_dirichlet.yaml"
-
-# job_name = "laser-polio-sk6"
-# study_name = "calib_nigeria_6y_2018_underwt_gravity_zinb_ipv_moreseeds_alt_wts_20250701"
-# model_config = "config_nigeria_6y_2018_underwt_gravity_zinb_ipv_moreseeds_alt.yaml"
-# calib_config = "r0_ssn_gravkabc_zinb_r0sclrs_siasclrs_initimmunsclrs_dirichlet_wts.yaml"
-
-# job_name = "laser-polio-sk7"
-# study_name = "calib_nigeria_6y_2018_underwt_gravity_zinb_ipv_wts_vxtranszero_20250703"
-# model_config = "config_nigeria_6y_2018_underwt_gravity_zinb_ipv_moreseeds_alt_zerovxtrans.yaml"
-# calib_config = "r0_ssn_gravkabc_zinb_r0sclrs_siasclrs_initimmunsclrs_dirichlet_wts.yaml"
-
-# job_name = "laser-polio-sk8"
-# study_name = "calib_nigeria_6y_2018_underwt_gravity_zinb_ipv_wts_vxtrans_20250703"
-# model_config = "config_nigeria_6y_2018_underwt_gravity_zinb_ipv_moreseeds_alt.yaml"
-# calib_config = "r0_ssn_gravkabc_zinb_r0sclrs_siasclrs_initimmunsclrs_dirichlet_wts.yaml"
-
-# job_name = "laser-polio-sk9"
-# study_name = "calib_nigeria_6y_2018_underwt_gravity_zinb_ipv_vxtrans_nsnga_20250707"
-# model_config = "config_nigeria_6y_2018_underwt_gravity_zinb_ipv_nsnga.yaml"
-# calib_config = "r0_ssn_gravkabc_zinb_r0sclrs_siasclrs_initimmunsclrs_dirichlet_wts_nsnga.yaml"
-
-# job_name = "laser-polio-sk10"
-# study_name = "calib_nigeria_6y_2018_underwt_gravity_zinb_ipv_vxtrans_nwecnga_20250708"
+# job_name = "laser-polio-sk11"
+# study_name = "calib_nigeria_6y_2018_underwt_gravity_zinb_ipv_vxtrans_nwecnga_regionaltimeseries_20250709"
 # model_config = "config_nigeria_6y_2018_underwt_gravity_zinb_ipv_nwecnga_3periods.yaml"
-# calib_config = "r0_ssn_gravkabc_zinb_r0sclrs_siasclrs_initimmunsclrs_dirichlet_wts_narrower.yaml"
+# calib_config = "r0_ssn_gravkabc_zinb_r0sclrs_siasclrs_initimmunsclrs_dirichlet_wts_narrower_regionaltimeseries.yaml"
 
-job_name = "laser-polio-sk11"
-study_name = "calib_nigeria_6y_2018_underwt_gravity_zinb_ipv_vxtrans_nwecnga_regionaltimeseries_20250709"
-model_config = "config_nigeria_6y_2018_underwt_gravity_zinb_ipv_nwecnga_3periods.yaml"
+job_name = "laser-polio-sk1"
+study_name = "calib_west_africa_7y_2017_underwt_20250715"
+model_config = "config_west_africa_7y_2017_underwt.yaml"
 calib_config = "r0_ssn_gravkabc_zinb_r0sclrs_siasclrs_initimmunsclrs_dirichlet_wts_narrower_regionaltimeseries.yaml"
 
 fit_function = "log_likelihood"

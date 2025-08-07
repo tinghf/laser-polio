1f4ede9d868bad87985c1b9271484cde68006922
skroiss-idm <steve.kroiss@gatesfoundation.org>
2025-08-07 11:44:23 -0700

    lowering num pods

--- calib/cloud/cloud_calib_config.py
+++ calib/cloud/cloud_calib_config.py
@@ -5,40 +5,15 @@ import yaml
 
 # ------------------- USER CONFIGS -------------------
 
-# job_name = "laser-polio-sk11"
-# study_name = "calib_nigeria_6y_2018_underwt_gravity_zinb_ipv_vxtrans_nwecnga_regionaltimeseries_20250709"
-# model_config = "config_nigeria_6y_2018_underwt_gravity_zinb_ipv_nwecnga_3periods.yaml"
-# calib_config = "r0_ssn_gravkabc_zinb_r0sclrs_siasclrs_initimmunsclrs_dirichlet_wts_narrower_regionaltimeseries.yaml"
-
-# job_name = "laser-polio-sk2"
-# study_name = "calib_west_africa_7y_2017_underwt_20250716"
-# model_config = "config_west_africa_7y_2017_underwt.yaml"
-# calib_config = "r0_ssn_gravkabc_zinb_r0sclrs_siasclrs_initimmunsclrs_dirichlet_wts_narrower_regionaltimeseries.yaml"
-
-# job_name = "laser-polio-sk3"
-# study_name = "calib_west_africa_7y_2017_underwt_20250723"
-# model_config = "config_west_africa_7y_2017_underwt.yaml"
-# calib_config = "r0_ssn_gravkabc_zinb_r0sclrs_siasclrs_initimmunsclrs_dirichlet_wts_narrower_regionaltimeseries.yaml"
-
-# job_name = "laser-polio-sk4"
-# study_name = "calib_west_africa_7y_2017_underwt_lwrmigr_20250724"
-# model_config = "config_west_africa_7y_2017_underwt_lwrmigr.yaml"
-# calib_config = "r0_ssn_gravkabc_zinb_r0sclrs_siasclrs_initimmunsclrs_dirichlet_wts_narrower_regionaltimeseries.yaml"
-
-# job_name = "laser-polio-sk5"
-# study_name = "calib_west_africa_7y_2017_underwt_radiation_20250724"
-# model_config = "config_west_africa_7y_2017_underwt_lwrmigr_radiation.yaml"
-# calib_config = "r0_ssn_rad_zinb_r0sclrs_siasclrs_initimmunsclrs_dirichlet_wts_narrower_regionaltimeseries.yaml"
-
-job_name = "laser-polio-sk6"
-study_name = "calib_west_africa_7y_2017_underwt_region_strategy_20250806"
+job_name = "lp-sk-1"
+study_name = "calib_west_africa_7y_2017_underwt_region_strategy_20250807"
 model_config = "config_west_africa_7y_2017_region_strategy.yaml"
 calib_config = "r0_ssn_gravkabc_zinb_r0sclrs_siasclrs_initimmunsclrs_dirichlet_wts_narrower_regionaltimeseries.yaml"
 
 fit_function = "log_likelihood"
 n_trials = 1  # Number of trials to run per pod
 n_replicates = 1  # Number of replicates to run for each trial
-parallelism = 50  # The number of pods (i.e., jobs) to run in parallel
+parallelism = 35  # The number of pods (i.e., jobs) to run in parallel
 completions = 10000  # The total number of pods (i.e., jobs) that need to successfully complete before the job is considered "done"
 
 # ---------------------------------------------------

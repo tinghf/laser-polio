2d6ed8c72a6e5e04e5038ee8a5a468b192952e9f
skroiss-idm <steve.kroiss@gatesfoundation.org>
2025-08-12 11:31:35 -0700

    run latest nigeria calib on lighter init branch

--- scripts/sandbox/run_calib_pars.py
+++ scripts/sandbox/run_calib_pars.py
@@ -18,29 +18,29 @@ typically printed during calibration runs.
 ######### USER PARAMETERS #########
 
 # Load model configuration from YAML file
-model_config_path = Path("calib/model_configs/config_west_africa_7y_2017_region_strategy.yaml")
+model_config_path = Path("calib/model_configs/config_nigeria_6y_2018_underwt_gravity_zinb_ipv_nwecnga_3periods.yaml")
 
 # Calib pars
 suggested_params = {
-    "r0": 8.498575531913849,
-    "seasonal_amplitude": 0.27275401111572795,
-    "seasonal_peak_doy": 246,
-    "gravity_k_exponent": -15.647094114805695,
-    "gravity_a": 1.7518681341407671,
-    "gravity_b": 1.0767329166378972,
-    "gravity_c": 1.13535137336246,
-    "node_seeding_zero_inflation": 0.958063878242425,
-    "r0_scalar_wt_slope": 53.9136355397035,
-    "r0_scalar_wt_intercept": 0.07092103447398954,
-    "r0_scalar_wt_center": 0.9974549294103241,
-    "sia_re_center": 0.16373623408279606,
-    "sia_re_scale": 0.4613139358510542,
-    "init_immun_scalar": 1.064316662988012,
+    "r0": 11.376375828469293,
+    "seasonal_amplitude": 0.06618410394577416,
+    "seasonal_peak_doy": 229,
+    "gravity_k_exponent": -8.896082688911651,
+    "gravity_a": 1.048239808810217,
+    "gravity_b": 0.7554399511356332,
+    "gravity_c": 0.459539396713007,
+    "node_seeding_zero_inflation": 0.6910803759617196,
+    "r0_scalar_wt_slope": 65.53324288590474,
+    "r0_scalar_wt_intercept": 0.10098281242018137,
+    "r0_scalar_wt_center": 0.40362946074304457,
+    "sia_re_center": 0.04566830306726448,
+    "sia_re_scale": 0.03337002867425776,
+    "init_immun_scalar": 1.0961955322491634,
 }
 
 # Runtime parameters
 verbose = 1
-results_path = "results/west_africa_calib_20250807"
+results_path = "results/nigeria_calib_20250812"
 
 ######### END OF USER PARS ########
 ###################################
@@ -50,6 +50,8 @@ with open(model_config_path) as f:
     model_config = yaml.safe_load(f)
 config = {**model_config, **suggested_params}
 config["results_path"] = results_path
+config["save_plots"] = True
+config["plot_pars"] = True
 
 # Run simulation
 sc.printcyan("🚀 Running simulation...")

e09ca1d84a3626378e0d4d81a6d3c844a0009c50
skroiss-idm <steve.kroiss@gatesfoundation.org>
2025-08-13 18:26:17 -0700

    new configs

--- calib/cloud/cloud_calib_config.py
+++ calib/cloud/cloud_calib_config.py
@@ -7,13 +7,13 @@ import yaml
 
 # Goal: Determine if the regional groupings allow us to calibrate to Nigeria.
 job_name = "lpsk1"
-study_name = "calib_nigeria_7y_2017_underwt_region_groupings_20250812"
+study_name = "calib_nigeria_7y_2017_underwt_region_groupings_20250813"
 model_config = "config_nigeria_7y_2017_region_groupings.yaml"
 calib_config = "r0_ssn_gravkabc_zinb_r0sclrs_siasclrs_initimmunsclrs_dirichlet_wts_narrower_regionaltimeseries.yaml"
 
 # # Goal: Determine if the adm01 groupings work
 # job_name = "lpsk2"
-# study_name = "calib_nigeria_7y_2017_underwt_adm01_groupings_20250812"
+# study_name = "calib_nigeria_7y_2017_underwt_adm01_groupings_20250813"
 # model_config = "config_nigeria_7y_2017_adm01_groupings.yaml"
 # calib_config = "r0_ssn_gravkabc_zinb_r0sclrs_siasclrs_initimmunsclrs_dirichlet_wts_narrower_regionaltimeseries.yaml"
 

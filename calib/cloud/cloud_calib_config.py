31d9626d12fcde3d9afdc66a9558815a54ff590d
skroiss-idm <steve.kroiss@gatesfoundation.org>
2025-08-19 16:50:33 -0700

    updating configs

--- calib/cloud/cloud_calib_config.py
+++ calib/cloud/cloud_calib_config.py
@@ -11,17 +11,23 @@ import yaml
 # model_config = "config_nigeria_7y_2017_region_groupings.yaml"
 # calib_config = "r0_k_ssn_wts.yaml"
 
-# Goal: switch to radiation_k_log10 & add max_migr_frac
-job_name = "lpsk6"
-study_name = "calib_nigeria_7y_2017_underwt_regions_maxmigrfrac_20250818"
-model_config = "config_nigeria_7y_2017_regions_sansmaxmigrfrac.yaml"
-calib_config = "r0_k_ssn_wts_maxmigrfrac.yaml"
+# # Goal: switch to radiation_k_log10 & add max_migr_frac
+# job_name = "lpsk6"
+# study_name = "calib_nigeria_7y_2017_underwt_regions_maxmigrfrac_20250818"
+# model_config = "config_nigeria_7y_2017_regions_sansmaxmigrfrac.yaml"
+# calib_config = "r0_k_ssn_wts_maxmigrfrac.yaml"
+
+# Goal: switch to radiation_k_log10 & add max_migr_frac, but set seasonal_peak_doy based on last calib
+job_name = "lpsk8"
+study_name = "calib_nigeria_7y_2017_underwt_regions_maxmigrfrac_setssnpeakdoy_20250818_forreal"
+model_config = "config_nigeria_7y_2017_regions_sansmaxmigrfrac_setssnpeakdoy.yaml"
+calib_config = "r0_k_ssnamp_wts_maxmigrfrac.yaml"
 
 fit_function = "log_likelihood"
 n_trials = 1  # Number of trials to run per pod
 n_replicates = 1  # Number of replicates to run for each trial
-parallelism = 100  # The number of pods (i.e., jobs) to run in parallel
-completions = 10000  # The total number of pods (i.e., jobs) that need to successfully complete before the job is considered "done"
+parallelism = 200  # The number of pods (i.e., jobs) to run in parallel
+completions = 5000  # The total number of pods (i.e., jobs) that need to successfully complete before the job is considered "done"
 
 # ---------------------------------------------------
 

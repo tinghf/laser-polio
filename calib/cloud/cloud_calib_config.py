ead768851f516d2ffbb8c82e16f6388751882715
skroiss-idm <steve.kroiss@gatesfoundation.org>
2025-08-26 10:50:58 -0700

    new configs

--- calib/cloud/cloud_calib_config.py
+++ calib/cloud/cloud_calib_config.py
@@ -13,13 +13,13 @@ import yaml
 
 # # Goal: try a lower r0
 # job_name = "lpsk2"
-# study_name = "calib_nigeria_7y_2017_underwt_regions_maxmigrfrac_dm_fix_lowerr0_20250825"
+# study_name = "calib_nigeria_7y_2017_underwt_regions_maxmigrfrac_dmfix_lowerr0_targetfix_20250826"
 # model_config = "config_nigeria_7y_2017_regions_sansmaxmigrfrac.yaml"
 # calib_config = "r0_k_ssn_wts_maxmigrfrac_lowerr0.yaml"
 
 # Goal: try a lower r0 & allow for zero-inflation
 job_name = "lpsk3"
-study_name = "calib_nigeria_7y_2017_underwt_regions_maxmigrfrac_dm_fix_lowerr0_zi_20250825"
+study_name = "calib_nigeria_7y_2017_underwt_regions_maxmigrfrac_dmfix_lowerr0_zi_targetfix_20250826"
 model_config = "config_nigeria_7y_2017_regions_sansmaxmigrfrac.yaml"
 calib_config = "r0_k_ssn_wts_maxmigrfrac_lowerr0_zi.yaml"
 

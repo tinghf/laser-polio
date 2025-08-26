a26ebac5f96d093a964b4320a72f19d476bae81b
skroiss-idm <steve.kroiss@gatesfoundation.org>
2025-08-25 17:32:11 -0700

    new configs

--- calib/cloud/cloud_calib_config.py
+++ calib/cloud/cloud_calib_config.py
@@ -5,22 +5,28 @@ import yaml
 
 # ------------------- USER CONFIGS -------------------
 
-# # Goal: switch to radiation_k_log10 & add max_migr_frac
-job_name = "lpsk1"
-study_name = "calib_nigeria_7y_2017_underwt_regions_maxmigrfrac_dm_fix_20250822"
-model_config = "config_nigeria_7y_2017_regions_sansmaxmigrfrac.yaml"
-calib_config = "r0_k_ssn_wts_maxmigrfrac.yaml"
+# # Goal: fit after fixing the DM issue
+# job_name = "lpsk1"
+# study_name = "calib_nigeria_7y_2017_underwt_regions_maxmigrfrac_dm_fix_lowerr0_20250825"
+# model_config = "config_nigeria_7y_2017_regions_sansmaxmigrfrac.yaml"
+# calib_config = "r0_k_ssn_wts_maxmigrfrac.yaml"
 
-# # Goal: Use the best pars from calib_nigeria_7y_2017_underwt_regions_maxmigrfrac_20250818 & sweep over seasonal_amplitude
-# job_name = "lpsk9"
-# study_name = "calib_nigeria_sweep_ssnamp_20250820"
-# model_config = "config_nigeria_7y_2017_region_r0_spd_radk_mmf.yaml"
-# calib_config = "ssnamp.yaml"
+# # Goal: try a lower r0
+# job_name = "lpsk2"
+# study_name = "calib_nigeria_7y_2017_underwt_regions_maxmigrfrac_dm_fix_lowerr0_20250825"
+# model_config = "config_nigeria_7y_2017_regions_sansmaxmigrfrac.yaml"
+# calib_config = "r0_k_ssn_wts_maxmigrfrac_lowerr0.yaml"
+
+# Goal: try a lower r0 & allow for zero-inflation
+job_name = "lpsk3"
+study_name = "calib_nigeria_7y_2017_underwt_regions_maxmigrfrac_dm_fix_lowerr0_zi_20250825"
+model_config = "config_nigeria_7y_2017_regions_sansmaxmigrfrac.yaml"
+calib_config = "r0_k_ssn_wts_maxmigrfrac_lowerr0_zi.yaml"
 
 fit_function = "log_likelihood"
 n_trials = 1  # Number of trials to run per pod
 n_replicates = 1  # Number of replicates to run for each trial
-parallelism = 200  # The number of pods (i.e., jobs) to run in parallel
+parallelism = 100  # The number of pods (i.e., jobs) to run in parallel
 completions = 5000  # The total number of pods (i.e., jobs) that need to successfully complete before the job is considered "done"
 
 # ---------------------------------------------------

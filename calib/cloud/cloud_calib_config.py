ffb77d83d141a8e9e012f6b298ea6c087aa1dbcc
skroiss-idm <steve.kroiss@gatesfoundation.org>
2025-09-08 16:56:56 -0700

    new configs

--- calib/cloud/cloud_calib_config.py
+++ calib/cloud/cloud_calib_config.py
@@ -5,22 +5,40 @@ import yaml
 
 # ------------------- USER CONFIGS -------------------
 
-# Goal: region calib after fixing the strain paralysis issue
-job_name = "lpsk3"
-study_name = "calib_nigeria_7y_2017_underwt_regions_maxmigrfrac_dmfix_lowerr0_zi_targetfix_paralysisfix_20250905"
-model_config = "config_nigeria_7y_2017_regions_sansmaxmigrfrac.yaml"
-calib_config = "r0_k_ssn_wts_maxmigrfrac_lowerr0_zi.yaml"
-
-# # Goal: adm01 calib after fixing the strain paralysis issue
-# job_name = "lpsk4"
-# study_name = "calib_nigeria_7y_2017_underwt_adm01_maxmigrfrac_dmfix_lowerr0_zi_targetfix_paralysisfix_20250905"
-# model_config = "config_nigeria_7y_2017_adm01_sansmaxmigrfrac.yaml"
+# # Goal: region calib after fixing the strain paralysis issue
+# job_name = "lpsk0"
+# study_name = "calib_nigeria_7y_2017_underwt_regions_maxmigrfrac_dmfix_lowerr0_zi_targetfix_paralysisfix_20250905"
+# model_config = "config_nigeria_7y_2017_regions_sansmaxmigrfrac.yaml"
 # calib_config = "r0_k_ssn_wts_maxmigrfrac_lowerr0_zi.yaml"
+
+# Goal: region calib after fixing the strain paralysis issue
+job_name = "lpsk1"
+study_name = "calib_nigeria_7y_2017_regions_gravity_ssn_nozi_pim_20250908"
+model_config = "nigeria_7y_2017_regions_gravity_ssn_nozi_pim.yaml"
+calib_config = "r0_gravk_mmf.yaml"
+
+# # Goal: region calib after fixing the strain paralysis issue
+# job_name = "lpsk2"
+# study_name = "calib_nigeria_7y_2017_regions_gravity_ssn_nozi_underwt_20250908"
+# model_config = "nigeria_7y_2017_regions_gravity_ssn_nozi_underwt.yaml"
+# calib_config = "r0_gravk_mmf.yaml"
+
+# # Goal: region calib after fixing the strain paralysis issue
+# job_name = "lpsk3"
+# study_name = "calib_nigeria_7y_2017_regions_radiation_ssn_nozi_pim_20250908"
+# model_config = "nigeria_7y_2017_regions_radiation_ssn_nozi_pim.yaml"
+# calib_config = "r0_radk_mmf.yaml"
+
+# # Goal: region calib after fixing the strain paralysis issue
+# job_name = "lpsk4"
+# study_name = "calib_nigeria_7y_2017_regions_radiation_ssn_nozi_underwt_20250908"
+# model_config = "nigeria_7y_2017_regions_radiation_ssn_nozi_underwt.yaml"
+# calib_config = "r0_radk_mmf.yaml"
 
 fit_function = "log_likelihood"
 n_trials = 1  # Number of trials to run per pod
 n_replicates = 1  # Number of replicates to run for each trial
-parallelism = 100  # The number of pods (i.e., jobs) to run in parallel
+parallelism = 50  # The number of pods (i.e., jobs) to run in parallel
 completions = 5000  # The total number of pods (i.e., jobs) that need to successfully complete before the job is considered "done"
 
 # ---------------------------------------------------
